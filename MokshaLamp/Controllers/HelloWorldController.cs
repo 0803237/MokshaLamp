4361816bd1f6cba7e02fc3917b2f7de62d3e87bc
0803237 <W0803237@myscc.ca>
2022-10-14 04:56:45 -0800

    Done till addinng controller file

---
+++ MokshaLamp/Controllers/HelloWorldController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Text.Encodings.Web;
+
+namespace MokshaLamp.Controllers
+{
+    public class HelloWorldController : Controller // class
+    {
+        public string Index() //Index Method
+        {
+            return "This is my default action...";
+        }
+
+        // GET: /HelloWorld/Welcome/ 
+
+        public string Welcome() 
+        {
+            return "This is the Welcome action method..."; // Welcome message
+        }
+    }
+}
--- MokshaLamp/README.txt
+++ MokshaLamp/README.txt
@@ -8,3 +8,12 @@ Assignment-1
 
 0437: Now I will build my project and run it to see if it successfully runs or not.
 0438: Yeh, Its running.
+
+0441: Created git repository and pushed this file in it.
+
+0443: Changed title to Moonlight lamps in Index.cshtml file under Home folder.
+
+0450: Modified background color
+
+==================== Adding a controller ===============================
+0456: Created HelloWorldController file in controller and changed its index and welcome method to display some message on screen
